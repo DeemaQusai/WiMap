d8700424b7f13e1dcefbbd7a2d42890a100d339c
DeemaQusai <deemaqusai@gmail.com>
2014-05-01 04:02:58 +0300

    small edit

--- WiMap2/scan.sh
+++ WiMap2/scan.sh
@@ -1,13 +1,4 @@
 #!bin/bash
 
 rm result.txt 2>/dev/null
-sudo iwlist scan 1> av_net 2> /dev/null
-
-while read line
-do
-	echo $line >> result.txt
-done < av_net
-
-rm -f file1 av_net
-
-
+sudo iwlist scan > result.txt
